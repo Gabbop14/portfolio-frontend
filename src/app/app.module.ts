a2b4571ce1ac0e9dd58fad05df9ec3864e46be32
Gabbo <g4briel.ruiz@gmail.com>
2022-12-16 21:55:30 -0300

    Import toolbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutMeComponent } from './sections/about-me/about-me.component';
 import { ExperienceComponent } from './sections/experience/experience.component';
-import { ToolbarComponent } from './ui/components/toolbar/toolbar.component';
+import { ToolbarComponent } from './shared/components/toolbar/toolbar.component';
 import { ProjectsComponent } from './sections/projects/projects.component';
 
 @NgModule({
