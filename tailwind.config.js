4eb9e72a47b026b1d9276ebf5fb350ceb2fe52d9
Gabbo <g4briel.ruiz@gmail.com>
2022-12-19 20:18:03 -0300

    Remove unnecesary old color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,20 +4,6 @@ module.exports = {
   darkMode: 'class',
   theme: {
     extend: {
-      colors: {
-        malibu: {
-          50: "#F6FCFF",
-          100: "#e0f4fe",
-          200: "#bae7fd",
-          300: "#7dd3fc",
-          400: "#38baf8",
-          500: "#0ea2e9",
-          600: "#0287c7",
-          700: "#036ea1",
-          800: "#075c85",
-          900: "#0c4e6e",
-        },
-      },
       fontFamily: {
         body: ["Quicksand", "Roboto Slab", "Roboto", "Helvetica Neue", "sans-serif"],
       },
