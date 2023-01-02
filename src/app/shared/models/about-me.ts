39fbbfecdd229b4c6704eca9300c5da700c6eed3
Gabbo <g4briel.ruiz@gmail.com>
2023-01-02 19:38:05 -0300

    Create model about me

---
+++ src/app/shared/models/about-me.ts
@@ -0,0 +1,10 @@
+export interface AboutMe{
+    banner_url              : string;
+    profile_url             : string;
+    full_name               : string;
+    current_position        : string;
+    current_company_icon_url: string;
+    current_company         : string;
+    current_address         : string;
+    detail                  : string;
+}
